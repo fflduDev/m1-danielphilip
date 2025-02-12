9b8519537dea7c9fb360e3e6839d68070c8a3f25
networkdd <daniel.davis1@student.fairfield.edu>
2025-02-12 14:55:38 -0500

    2/12 uptated queue

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -1,40 +1,69 @@
 package queue;
 
 public class QueueImpl implements Queue {
-
+int Length = 6;
+Strig[] arr = new Strig[Length];
+int size = 0;
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
+		
+		if(arr[Length-1] != null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
+		if(arr[0] == null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void enQueue(String element) {
-		// TODO Auto-generated method stub
 		
+		if(isFull()) {
+			System.out.println("Queue is full");
+		}
+		else {
+			arr[size] = element;
+			size++;
+		}
 	}
 
 	@Override
 	public String deQueue() {
-		// TODO Auto-generated method stub
+		
+		if(!isEmpty()) {
+			String temp = arr[0];
+			for(int i = 1; i < size; i++) {
+				arr[i-1] = arr[i];
+			}
+			arr[size-1] = null;
+			size--;
+			return temp;
+		}
 		return null;
 	}
 
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println(arr[i]);
+		}
 		
 	}
 
 	@Override
 	public String peek() {
-		// TODO Auto-generated method stub
+		
+		if(!isEmpty()) {
+			return arr[0];
+		}
 		return null;
 	}
 
