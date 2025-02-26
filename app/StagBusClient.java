b4286760733961eae4273bcad44f556777b5d7b2
networkdd <daniel.davis1@student.fairfield.edu>
2025-02-26 18:08:47 -0500

    updated StagbusClient

--- app/StagBusClient.java
+++ app/StagBusClient.java
@@ -17,8 +17,42 @@ public class StagBusClient {
 		//listRunTestMethod...
  		
 		System.out.println("-----S T A C K  T E S T------");
+		Stack stackTester = new StackImpl(10);
 
-		//QRunTestMethod...
+		stackTester.push("Alice");
+		stackTester.push("Bob");
+		stackTester.push("Charlie");
+		stackTester.push("David");
+		stackTester.push("Eve");
+		stackTester.push("Frank");
+
+		stackTester.display();
+
+		// Peek at the stack
+		System.out.println("Peek: " + stackTester.peek());
+
+		// Remove (pop) the top of the stack
+		System.out.println("Popped: " + stackTester.pop());
+
+		stackTester.display();
+
+		// Peek at the stack
+		System.out.println("Peek: " + stackTester.peek());
+
+		// 2 more riders
+		stackTester.push("Grace");
+		stackTester.push("Hank");
+
+		// Peek at the stack
+		System.out.println("Peek: " + stackTester.peek());
+
+		// Remove all
+		while (!stackTester.isEmpty()) {
+			System.out.println("Popped: " + stackTester.pop());
+		}
+
+		System.out.println("Is stack empty? " + stackTester.isEmpty());
+
 	 
 		System.out.println("----Q U E U E  T E S T-------");
 		
